33b1d94914960c04851b710905b6f00cf5f6deef
Andrey Yarysh <and@yarysh.com>
2025-07-30 20:34:38 +0200

    Set firefox as default browser

--- install/apps/mimetypes.sh
+++ install/apps/mimetypes.sh
@@ -13,10 +13,10 @@ xdg-mime default imv.desktop image/tiff
 # Open PDFs with the Document Viewer
 xdg-mime default org.gnome.Evince.desktop application/pdf
 
-# Use Chromium as the default browser
-xdg-settings set default-web-browser chromium.desktop
-xdg-mime default chromium.desktop x-scheme-handler/http
-xdg-mime default chromium.desktop x-scheme-handler/https
+# Use Firefox as the default browser
+xdg-settings set default-web-browser firefox.desktop
+xdg-mime default firefox.desktop x-scheme-handler/http
+xdg-mime default firefox.desktop x-scheme-handler/https
 
 # Open video files with mpv
 xdg-mime default mpv.desktop video/mp4
