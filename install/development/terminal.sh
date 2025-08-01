bdea4c2299ea1ab44eb712af743fec9d0178d131
Andrey Yarysh <and@yarysh.com>
2025-08-01 22:37:17 +0200

    Change bash to zsh

--- install/development/terminal.sh
+++ install/development/terminal.sh
@@ -4,5 +4,6 @@ yay -S --noconfirm --needed \
   wget curl unzip inetutils impala \
   fd eza fzf ripgrep zoxide bat jq \
   wl-clipboard fastfetch btop \
-  man tldr less whois plocate bash-completion \
+  man tldr less whois plocate \
+  zsh zsh-syntax-highlighting zsh-autosuggestions \
   alacritty
